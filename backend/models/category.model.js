b9ab6bb2e9f1c9c6d9dab5052317d95cd7208cfa
Adeeshp <adeeshpatel@outlook.com>
2024-10-05 15:24:57 -0400

    add new model for category and subcategory, update user modal

---
+++ backend/models/category.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import AutoIncrementFactory from 'mongoose-sequence';
+
+// Initialize AutoIncrement with mongoose
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+const categorySchema = new mongoose.Schema({
+    categoryId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    categoryName: {
+        type: String,
+        required: true
+    },
+    imageURL: {
+        type: String
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true 
+});
+
+// Apply AutoIncrement plugin to categoryId
+categorySchema.plugin(AutoIncrement, { inc_field: 'categoryId' });
+
+// Model
+const Category = mongoose.model('Category', categorySchema);
+export default Category;
