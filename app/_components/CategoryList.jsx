7132a2dd718155d75745ae12e91b0f9e581bd8e7
Keval Patel <kvpatel1682001@gmail.com>
2024-12-14 17:49:09 -0500

    task proposal flow updated

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Task from '@/backend/models/task.model';
 
 function CategoryList({ categoryList }) {
   return (
     <div className='mx-4 md:mx-22 lg:mx-52 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4'>
       {categoryList.length > 0 ? categoryList.map((category, index) => (
-        <Link href={'/search/' + category.categoryName} key={index} className={`flex flex-col items-center
+        <Link href="task-creation" key={index} className={`flex flex-col items-center
              justify-center gap-2
              bg-blue-50 p-5 rounded-lg
              cursor-pointer hover:bg-blue-100 hover:scale-110 transition-all ease-in-out
@@ -21,16 +22,15 @@ function CategoryList({ categoryList }) {
         </Link>
       )) : 
         [1,2,3,4,5,6].map((item,index)=>(
-            // <div key={index} className='h-[120px] w-full bg-slate-200 animate-pulse rounded-lg'></div>
-            <div key={index} className="w-full h-[105px] bg-slate-200 rounded-lg animate-pulse flex items-center justify-center">
-              <Image
-                src="/fixn.svg"
-                alt="fixn-logo"
-                width={60}
-                height={60}
-                className="filter grayscale"
-              />
-            </div>
+          <div key={index} className="w-full h-[105px] bg-slate-200 rounded-lg animate-pulse flex items-center justify-center">
+            <Image
+              src="/fixn.svg"
+              alt="fixn-logo"
+              width={60}
+              height={60}
+              className="filter grayscale"
+            />
+          </div>
         ))
       }
     </div>
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -20,7 +20,7 @@ router.get('/tasks', getAllTasks); // Get a list of all tasks (viewable by all s
 router.get('/task/:userId', getTaskByUserId); // Get details of a specific task by its User ID
 
 // **PUT /api/task/:taskId**
-router.post('/task/:taskId', updateTask); // Update details of a specific task by its ID
+router.post('/task/updateTask/:taskId', updateTask); // Update details of a specific task by its ID
 
 // **DELETE /api/task/:taskId**
 router.delete('/task/:taskId', deleteTask); // Delete a task by its ID
